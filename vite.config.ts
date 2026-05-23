1998e7467c0e083f31d9c6b18d73b24b016eaac2
snehakapilavayi <snehakapilavayi07@gmail.com>
2026-05-23 21:07:09 +0530

    chore: set preset to netlify

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ command }) => {
       tailwindcss(),
       tanstackStart({
         server: {
-          preset: "vercel" 
+          preset: "netlify" 
         },
       }),
       react(),
